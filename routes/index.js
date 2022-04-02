a69ed939be94e8b7fdd302a1f1c51b607469afca
Gustavo Velozo <gvnv.2000@gmail.com>
2022-04-02 20:00:51 -0300

    Table Jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', list:[[1,2],[3,4]] });
+});
+router.get('/val', function(req, res, next) {
+  res.render('index', { title: 'VALORANT', list:["batata", "carne", "abobrinha"] });
 });
 
 module.exports = router;
