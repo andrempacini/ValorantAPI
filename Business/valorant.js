faf42014a768b098d31a9a290d5c89089de3bd49
Gustavo Velozo <gvnv.2000@gmail.com>
2022-04-03 03:42:22 -0300

    pag atos e rota p ato

---
+++ Business/valorant.js
@@ -0,0 +1,63 @@
+
+exports.getActs = async () => {
+
+    const axios = require('axios')
+    let res
+    try {
+        res = await axios({
+            method: "get",
+            url: "https://br.api.riotgames.com/val/content/v1/contents?locale=pt-BR",
+            headers: {
+                "X-Riot-Token": "RGAPI-9649fa17-41b6-4594-af29-5617784d2a8a"
+            }
+        })
+    } catch {
+        res = "deu ruim"
+    }
+
+    resp = res?.data?.acts?.length > 0;
+    if (resp) {
+        retorno = res.data.acts;
+    } else {
+        retorno = "Credenciais inválidas";
+    }
+
+    console.log(`statusCode: ${res.status}`)
+    return retorno
+}
+
+
+exports.getLeaderboard = async (acts) => {
+
+    const axios = require('axios')
+    let res
+    //console.log("gothere")
+    try {
+        res = await axios({
+            method: "get",
+            url: "https://br.api.riotgames.com/val/ranked/v1/leaderboards/by-act/" + acts,
+            headers: {
+                "X-Riot-Token": "RGAPI-9649fa17-41b6-4594-af29-5617784d2a8a"
+            }
+        })
+    } catch {
+        res = "deu ruim"
+    }
+
+    resp = res?.data?.players?.length > 0;
+    if (resp) {
+        retorno = res.data.players;
+        console.log(retorno.map(x=>x.puuid))
+        retorno = retorno.map(x => ({
+            rank: x.leaderboardRank,
+            name: x.gameName ?? "ANONIMO",
+            points: x.rankedRating,
+        }));
+    } else {
+        retorno = "Credenciais inválidas";
+    }
+
+    console.log(`statusCode: ${res.status}`)
+
+    return retorno;
+}
