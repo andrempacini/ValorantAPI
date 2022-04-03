e0db35a97783e72cc9a5fb5cab2d5550bdda1772
chpdss <64985566+chpdss@users.noreply.github.com>
2022-04-02 23:58:31 -0300

    design de milhoes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', list:[[1,2],[3,4]] });
+  res.render('index', { title: 'vava', list:[[1,2,"andre"],[3,4,"xapadss"]] });
 });
 router.get('/val', function(req, res, next) {
   res.render('index', { title: 'VALORANT', list:["batata", "carne", "abobrinha"] });
